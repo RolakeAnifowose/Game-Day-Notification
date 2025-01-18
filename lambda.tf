c3686dad2a14a9b0f07981e818e46bdb3388334e
Rolake <morolaanney@gmail.com>
2025-01-18 13:25:58 +0000

    Added NBA api key from sports data

--- lambda.tf
+++ lambda.tf
@@ -1,3 +1,14 @@
-# resource "aws_lambda_function" "game-day-lambda" {
-#     function_name = "game-day-lambda"
-# }
+resource "aws_lambda_function" "game-day-lambda" {
+    function_name = "game-day-lambda"
+    runtime = "python3.13"
+    role = aws_iam_role.game-day-lambda-role.arn
+    filename = "notifications.zip"
+    handler = "notifications.lambda_handler"
+
+    environment {
+        variables = {
+            NBA_API_KEY = "7a56c2fc2b78462e840b340a20869600"
+            SNS_TOPIC_ARN = "arn:aws:sns:us-east-1:556298987240:game-day-notification"
+        }
+    }
+}
