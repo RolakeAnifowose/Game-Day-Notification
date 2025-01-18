5c7156f9ef716242fea446de1212b8eb9c069e79
Rolake <morolaanney@gmail.com>
2025-01-18 12:59:31 +0000

    Added lambda role

--- iam.tf
+++ iam.tf
@@ -14,3 +14,21 @@ resource "aws_iam_policy" "game-day-iam-policy" {
         ]
     })
 }
+
+resource "aws_iam_role" "game-day-lambda-role" {
+    name = "game-day-lambda-role"
+    managed_policy_arns = [aws_iam_policy.game-day-iam-policy.arn, "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"]
+
+    assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
