b444f5e82eeb836b4b101e9d17ed87b6ea3e548f
Rolake <morolaanney@gmail.com>
2025-01-18 12:18:02 +0000

    Added email endpoint

--- sns.tf
+++ sns.tf
@@ -5,4 +5,5 @@ resource "aws_sns_topic" "game-day-notification" {
 resource "aws_sns_topic_subscription" "game-day-subscription" {
     topic_arn = "arn:aws:sns:us-east-1:556298987240:game-day-notification"
     protocol = "email"
+    endpoint = "morolaanney@gmail.com"
 }
