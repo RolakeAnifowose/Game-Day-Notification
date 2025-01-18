bbc5fe35c00546f6012d5d7e145a1e8972df9854
Rolake <morolaanney@gmail.com>
2025-01-18 12:12:42 +0000

    Added the file for sns and pipeline

---
+++ sns.tf
@@ -0,0 +1,8 @@
+resource "aws_sns_topic" "game-day-notification" {
+    name = "game-day-notification"
+}
+
+resource "aws_sns_topic_subscription" "game-day-subscription" {
+    topic_arn = "arn:aws:sns:us-east-1:556298987240:game-day-notification"
+    protocol = "email"
+}
