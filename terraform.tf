6c767686c25729801a3e13b4f5bae3a8831486aa
Rolake <morolaanney@gmail.com>
2025-01-18 13:39:50 +0000

    Added S3 bucket for state file

---
+++ terraform.tf
@@ -0,0 +1,19 @@
+terraform {
+    backend "s3" {
+      bucket = "game-day-notification-morolake"
+      key = "game-day-notification-morolake/terraform.tfstate"
+      region = "us-east-1"
+      #dynamodb_table = "terraform-state-table"
+    }
+}
+
+resource "aws_s3_bucket" "game-day-notification-morolake" {
+    name = "game-day-notification-morolake"
+}
+
+# resource "aws_dynamodb_table" "terraform-state-table" {
+#     name = "terraform-state-table"
+#     billing_mode = "PROVISIONED"
+#     write_capacity = 10
+#     read_capacity = 10
+# }
